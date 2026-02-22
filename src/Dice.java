10e0ccd149f1d92f09869c5a7ccf2d7e9cb251dd
Manu-aah <mg770@sussex.ac.uk>
2026-02-22 18:08:52 +0000

    Dice class with rollDice method

---
+++ src/Dice.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Dice {
+    Random random = new Random();
+    int total;
+
+    public int rollDice(int numOfDice) {
+        total = 0;
+        for (int i = 0; i < numOfDice; i++){
+            total += random.nextInt(1,7);
+        }
+        return total;
+    }
+}
