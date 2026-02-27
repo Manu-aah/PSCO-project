6ff0856c9323d23bd9b16b43b13821a9f4036d19
Manu-aah <mg770@sussex.ac.uk>
2026-02-27 18:10:37 +0000

    simplified some logic and added some error handling for scanner.

--- src/Game_4.java
+++ src/Game_4.java
@@ -5,59 +5,73 @@ public class Game_4 {
     int dealerTotal;
     ArrayList<Integer> playerDice;
     int playerTotal;
-    int minimumStake;
+    int Stake;
     Dice dice = new Dice();
 
-    public Game_4(int minimumStake){
-        this.minimumStake = minimumStake;
+    public Game_4(int Stake){
+        this.Stake = Stake;
         playerDice = new ArrayList<>();
         dealersDice = new ArrayList<>();
     }
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
-        boolean bust = false;
+        boolean finished = false;
 
         System.out.println("Rolling dice....");
         playerDice.add(dice.rollDice(1));
-
-        System.out.println("\nFirst DIce: " + playerDice.getFirst());
+        System.out.println("First dice: " + playerDice.get(0));
         playerDice.add(dice.rollDice(1));
-        System.out.println("\nSecond Dice:" + playerDice.get(1));
+        System.out.println("Second dice:" + playerDice.get(1));
         playerTotal = playerDice.get(0) + playerDice.get(1);
+        System.out.println("Player total: " + playerTotal);
 
-        System.out.println("\nDealers turn....");
+        System.out.println("Dealers turn....");           //dealers turn
         System.out.println("Rolling dice....");
         dealersDice.add(dice.rollDice(1));
-        System.out.println("\nFirst DIce: " + dealersDice.getFirst());
+        System.out.println("First dice: " + dealersDice.get(0));
         dealersDice.add(dice.rollDice(1));
-        System.out.println("\nSecond Dice:" + dealersDice.get(1));
+        System.out.println("Second dice:" + dealersDice.get(1));
         dealerTotal = dealersDice.get(0) + dealersDice.get(1);
+        System.out.println("Dealer total: " + dealerTotal);
 
         int i = 2;
-        while(!bust){
-            int choice = scanner.nextInt();
-            if (choice > 0 && choice < 3) {
-                switch (choice) {
-                    case 1:
-                        System.out.println("Hit!");
-                        playerDice.add(dice.rollDice(1));
-                        playerTotal += playerDice.get(i);
-                        System.out.println(playerTotal);
-                        i++;
-                        if (playerTotal > 21){
-                            System.out.println("BUST!");
-                            bust = true;
-                        };
-                        break;
-                    case 2:
-                        System.out.println("Stand!");
-                        dealersTurn();
-                        if(dealerTotal>playerTotal && dealerTotal<=21) System.out.println("YOU LOSE!!!");
-                        else System.out.println("YOU WIN!!!");
-                        break;
-                }
-        } else;
+        while (!finished) {
+            System.out.println("HIT OR STAND?\nPress 1 to hit or 2 to stand.");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid choice! Please enter 1 or 2.");
+                scanner.next();     //this ignored the invalid input and moves to the next input. This is for a simple int validation step
+                continue;
             }
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println("HIT!!!");
+                playerDice.add(dice.rollDice(1));
+                int currentRoll = playerDice.get(i);
+                playerTotal += currentRoll;
+                System.out.println("You rolled: " + currentRoll);
+                System.out.println("Player total: " + playerTotal);
+                i++;
+                if (playerTotal>21) {
+                    System.out.println("BUST! YOU LOSE!");
+                    finished = true;
+                }
+
+            } else if (choice == 2) {
+                System.out.println("STAND!!!");
+                dealersTurn();
+                if(dealerTotal>21){
+                    System.out.println("DEALER BUST! YOU WIN!");
+                } else if(dealerTotal>playerTotal){
+                    System.out.println("YOU LOSE!!! Dealers roll is closer to 21");
+                } else System.out.println("YOU WIN!!!");
+                finished = true;
+            } else {
+                throw new IllegalArgumentException("Invalid choice! Please enter 1 or 2.");
+            }
+        } scanner.close();
+        System.out.println("Game Over!");
     }
     public void dealersTurn(){
         int i = 2;
